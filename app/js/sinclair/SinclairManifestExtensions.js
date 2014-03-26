2f0a6c5944b0441546faa6bea81ce14d23b5a17a
nwebber <Nathan.Weber@mlb.com>
2014-03-26 14:48:47 -0500

    use a fancy regex to figure out the index

--- app/js/sinclair/SinclairManifestExtensions.js
+++ app/js/sinclair/SinclairManifestExtensions.js
@@ -5,7 +5,7 @@ MediaPlayer.sinclair.SinclairManifestExtensions = function () {
     this.getPresentationOffset = function (manifest, periodIndex) {
         var deferred = Q.defer(),
         	req = new XMLHttpRequest(),
-        	ENDPOINT = "http://localhost/~nweber/sinclair/server.json";
+        	ENDPOINT = "http://localhost/~nweber/dash.js/server.json";
 
 		req.open("GET", ENDPOINT, true);
 		req.responseType = "json";
@@ -13,13 +13,56 @@ MediaPlayer.sinclair.SinclairManifestExtensions = function () {
 		req.onload = function () {
 			var data = req.response,
 				f = data.fragment,
-				fDuration,
-            	fTimescale = 1,
-				idx = f.substring(f.lastIndexOf("/") + 1, f.lastIndexOf(".")),
 				t = new Date(new Date().getTime() - 3000); /// three seconds ago //new Date(data.time);
+			
+			f = f.substring(f.lastIndexOf("/")+1);
+			f = f.substring(0, f.lastIndexOf("."));
 
-			var representation = manifest.Period_asArray[periodIndex].AdaptationSet_asArray[1].Representation_asArray[0],
+			var adaptations = manifest.Period_asArray[periodIndex].AdaptationSet_asArray,
+				wildcards,
+				matches,
+				template;
+
+			for (var j = 0; j < adaptations.length; j++) {
+				// get segment template
+				var representation = adaptations[j].Representation_asArray[0];
+				
 				template = representation.SegmentTemplate;
+
+				// figure out index from fragment url
+				var media = template.media.substring(0, template.media.lastIndexOf("."));
+				
+				var w = /\$[a-zA-Z]*\$/g,
+					reg = new RegExp(media.replace(w, "(\\d+)"));
+					
+				wildcards = media.match(w)
+				matches = reg.exec(f);
+
+				if (matches !== null) {
+					break;
+				}
+			}
+
+			var indexIdx,
+				idx,
+				k;
+
+			for (k = 0; k < wildcards.length; k++) {
+				if (wildcards[k] === "$Number$") {
+					indexIdx = k;
+					break;
+				}
+			}
+
+			idx = parseInt(matches[k+1]);
+
+			if (template.hasOwnProperty("startNumber")) {
+				idx -= template.startNumber;
+			}
+
+			// figure out time based on index
+			var fDuration,
+            	fTimescale = 1;
 
 			if (template.hasOwnProperty("timescale")) {
                 fTimescale = template.timescale;
